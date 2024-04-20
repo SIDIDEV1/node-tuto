fda5498a45da7b92e3b2bc08f5a3f6712fa3c9dd
SIDIDEV <sidimariko61@gmail.com>
2024-04-20 15:55:29 +0100

    Promise all usecase

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import { readFile } from 'node:fs/promises'
 
-const content = await readFile('text.txt', { encoding: 'utf8' })
+const content = await Promise.all([
+    readFile('text.txt', { encoding: 'utf8' }),
+    readFile('app.js', { encoding: 'utf8' })
+])
 
-console.log(content);
 console.log('content');
+console.log(content);
