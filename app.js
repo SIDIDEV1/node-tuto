f36b1b1b6a88b44f2ee81e5671bdeafd48e433e3
SIDIDEV <sidimariko61@gmail.com>
2024-04-21 15:27:52 +0100

    CRUD api server with show list

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-import { createReadStream } from 'node:fs'
 import { createServer } from 'node:http'
+import { findTodos } from './functions/todos_storage.js'
 
-
-const server = createServer((req, res) => {
-    const file = createReadStream('index.html')
-    file.pipe(res)
-})
-
-server.listen('8888')
+createServer(async (req, res) => {
+    res.setHeader('Content-Type', 'application/json')
+    const url = new URL(req.url, `http://${req.headers.host}`)
+    if (url.pathname === '/todos') {
+        const todos = await findTodos()
+        res.write(JSON.stringify(todos))
+    } else {
+        res.writeHead(404)
+    }
+    res.end()
+}).listen(3000)
