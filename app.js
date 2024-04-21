15bb7897e898b2298ccf5b1ea9a6556ec22f01c5
SIDIDEV <sidimariko61@gmail.com>
2024-04-21 18:39:18 +0100

    HandleError api

--- app.js
+++ app.js
@@ -1,32 +1,40 @@
 import { createServer } from 'node:http'
 import { create, index, remove } from './functions/api/todo.js'
+import { NotFoundError } from './functions/errors.js'
 
 createServer(async (req, res) => {
-    res.setHeader('Content-Type', 'application/json')
-    const url = new URL(req.url, `http://${req.headers.host}`)
-    console.log(url);
-    const endpoint = `${req.method}:${url.pathname}`
-    let results
+    try {
+        res.setHeader('Content-Type', 'application/json')
+        const url = new URL(req.url, `http://${req.headers.host}`)
+        console.log(url);
+        const endpoint = `${req.method}:${url.pathname}`
+        let results
 
-    switch (endpoint) {
-        case 'GET:/todos':
-            results = await index(req, res)
-            break;
+        switch (endpoint) {
+            case 'GET:/todos':
+                results = await index(req, res)
+                break;
 
-        case 'POST:/todos':
-            results = await create(req, res)
-            break;
+            case 'POST:/todos':
+                results = await create(req, res)
+                break;
 
-        case 'DELETE:/todos':
-            results = await remove(req, res, url)
-            break;
-        default:
+            case 'DELETE:/todos':
+                results = await remove(req, res, url)
+                break;
+            default:
+                res.writeHead(404);
+        }
+
+        if (results) {
+            res.write(JSON.stringify(results))
+        }
+    } catch (error) {
+        if (error instanceof NotFoundError) {
             res.writeHead(404);
+        } else {
+            throw error
+        }
     }
-
-    if (results) {
-        res.write(JSON.stringify(results))
-    }
-
     res.end()
 }).listen(3000)
